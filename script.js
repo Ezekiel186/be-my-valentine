abca1acb231617d90d56034b77dd69951122c8b0
Ezekiel <ezekieljamolin186@gmail.com>
2024-02-03 21:03:58 -0800

    added audio

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.querySelector('.yes').addEventListener('click', function(){
+    document.getElementById('happy').play();
     document.querySelector('.image').src = "./images/happy.gif";
 });
 
 var no = document.querySelector('.no');
 
 no.addEventListener('click', function(){
-    var X = Math.floor(Math.random() * 500) - 100;
-    var Y = Math.floor(Math.random() * 500) - 100;
+    var X = Math.floor(Math.random() * 400) - 100;
+    var Y = Math.floor(Math.random() * 400) - 100;
 
     no.style.transform = `translate(${X}px, ${Y}px)`;
     no.classList.add('clicked');
