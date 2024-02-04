449cabd8eb89c8bacb4421925d5f107477a93847
Ezekiel <ezekieljamolin186@gmail.com>
2024-02-03 18:25:33 -0800

    changed how no button works

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.querySelector('.yes').addEventListener('click', function(){
-    document.querySelector('.image').src = "./images/happy.gif"
+    document.querySelector('.image').src = "./images/happy.gif";
+});
+
+var no = document.querySelector('.no');
+
+no.addEventListener('click', function(){
+    var X = Math.floor(Math.random() * 500) - 100;
+    var Y = Math.floor(Math.random() * 500) - 100;
+
+    no.style.transform = `translate(${X}px, ${Y}px)`;
+    no.classList.add('clicked');
+    setTimeout(()=> {
+            GamepadButton.classList.remove('clicked');
+        }, 300)
 });
